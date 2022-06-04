6412f6c93ce3dadea18cfc77e708e77e1bf6a2db
justEstif <95101485+justEstif@users.noreply.github.com>
2022-06-04 16:25:05 -0400

    first commit + first test

---
+++ src/App.test.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { render, screen } from '@testing-library/react';
+import "@testing-library/jest-dom"
+// import userEvent from "@testing-library/user-event"
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/hello/i);
+  expect(linkElement).toBeInTheDocument();
+});
