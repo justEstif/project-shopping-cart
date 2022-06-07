1234bde6eb40264b3a13eb5ea8e702f58bba8586
justEstif <95101485+justEstif@users.noreply.github.com>
2022-06-07 04:41:40 -0400

    shared state of items in cart between App and Item

--- src/App.js
+++ src/App.js
@@ -7,40 +7,20 @@ import { useState } from "react";
 const App = () => {
   const [clickCount, setClickCount] = useState(0);
   const [items, setItems] = useState([]);
-  const handleAddToCart = (newItem) => {
-    const matchingName = (value) => newItem.name === items.value.name;
-    let tempMatch = items.filter(matchingName)
+  const handleAddToCart = ({ name, count, price }) => {
     let tempItems = items;
-    // let tempIndex;
-    // if (tempIndex >= 0) {
-    //   tempMatch = tempItems[tempIndex];
-    //   tempMatch.count += newItem.count;
-    //   tempItems[tempIndex] = tempMatch;
-    // } else tempItems.push(newItem);
-    //
-    // setItems(tempItems)
-    tempItems.push(newItem);
-    console.log(tempItems);
-    console.log(`tempMatch: ${tempMatch}`)
+    let tempIndex;
+    for (let i = 0; i < tempItems.length; i++) {
+      if (tempItems[i].name == name) {
+        tempIndex = i;
+      }
+    }
+    tempIndex >= 0
+      ? (tempItems[tempIndex].count = tempItems[tempIndex].count + count)
+      : tempItems.push({ name, count, price });
 
-    // let tempMatch; let matchIndex;
-    // let tempState = items;
-    // for(const item of items)
-    // for (let i = 0; items.length; i++) {
-    //   if (items[i].name === newItem.name) {
-    //     matchIndex = i;
-    //     tempMatch = items[i]
-    //   }
-    // }
-    // if (tempMatch) {
-    //   tempMatch.count += newItem.count;
-    //   tempState[matchIndex] = tempMatch;
-    // } else {
-    //   tempState.push(newItem);
-    // }
-    // setItems(tempState);
-    // setClickCount(items.length);
-    // console.log(newItem);
+    setItems(tempItems);
+    setClickCount(tempItems.length);
   };
   return (
     <Router>
